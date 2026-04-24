b5290d9e10246a66b3d1a75b5942b78eb242a99d
Mark Antonov <antonov.mark@icloud.com>
2026-04-24 21:38:31 +0300

    Harden properties dialog loading to prevent 500 on open

---
+++ local/activities/custom/taskchatsendmessage/lang/ru/taskchatsendmessage.php
@@ -0,0 +1,13 @@
+<?php
+$MESS['TASKCHATSENDMESSAGE_ERROR_TASKS_MODULE'] = 'Не удалось подключить модуль tasks.';
+$MESS['TASKCHATSENDMESSAGE_ERROR_IM_MODULE'] = 'Не удалось подключить модуль im.';
+$MESS['TASKCHATSENDMESSAGE_ERROR_TASK_ID'] = 'Некорректный ID задачи.';
+$MESS['TASKCHATSENDMESSAGE_ERROR_TASK_NOT_FOUND'] = 'Задача с ID #TASK_ID# не найдена.';
+$MESS['TASKCHATSENDMESSAGE_ERROR_SENDER_ID'] = 'Некорректный отправитель.';
+$MESS['TASKCHATSENDMESSAGE_ERROR_SENDER_NOT_FOUND'] = 'Пользователь с ID #USER_ID# не найден.';
+$MESS['TASKCHATSENDMESSAGE_ERROR_EMPTY_MESSAGE'] = 'Текст сообщения не может быть пустым.';
+$MESS['TASKCHATSENDMESSAGE_ERROR_TASK_IM_INTEGRATION'] = 'Класс интеграции задач и чатов недоступен.';
+$MESS['TASKCHATSENDMESSAGE_ERROR_CHAT_NOT_FOUND'] = 'Не удалось получить или создать чат задачи.';
+$MESS['TASKCHATSENDMESSAGE_ERROR_SEND_FAILED'] = 'Не удалось отправить сообщение в чат задачи.';
+$MESS['TASKCHATSENDMESSAGE_TRACKING_SUCCESS'] = 'Сообщение отправлено: задача #TASK_ID#, чат #CHAT_ID#, сообщение #MESSAGE_ID#.';
+$MESS['TASKCHATSENDMESSAGE_TRACKING_ERROR'] = 'Ошибка отправки сообщения в чат задачи: #ERROR#';
